db303e4923dd10c521954b29c2966fb36a55804f
Vlad <v.kilchevskii@ostrovchistoty.by>
2021-06-24 17:33:26 +0300

    ac

--- todo/src/components/Tasks/index.jsx
+++ todo/src/components/Tasks/index.jsx
@@ -1,18 +1,39 @@
   
 import React from 'react';
-import editSvg from '../../assets/img/edit.svg';
+import axios from 'axios'
+import AddTaskForm from './addTaskForm'
 
+
+import editSvg from '../../assets/img/edit.svg';
 import './Tasks.scss';
 
-const Tasks = ({ list }) => {
+
+
+const Tasks = ({ list, onEditTitle, onAddTask, withoutEmpty }) => {
+
+  const editTitle = () => {
+    const newTitle = window.prompt('Название списка', list.name);
+    if (newTitle) {
+      onEditTitle(list.id, newTitle);
+      axios.patch('http://localhost:3001/lists/' + list.id, {
+        name: newTitle
+      }).catch(() => {
+        alert('Не удалось обновить название')
+      });
+    }
+  };
+
   return (
     <div className="tasks">
-      <h2 className="tasks__title">
+      <h2 style={ {color: list.color.hex} } className="tasks__title">
         {list.name}
-        <img src={editSvg} alt="Edit icon" />
+        <img onClick={editTitle} 
+        src={editSvg} 
+        alt="Edit icon" />
       </h2>
 
       <div className="tasks__items">
+        {!withoutEmpty && !list.tasks.length && <h2>Задачи отсутствуют</h2>}
         {list.tasks.map(task => (
           <div key={task.id} className="tasks__items-row">
             <div className="checkbox">
@@ -38,6 +59,7 @@ const Tasks = ({ list }) => {
             <input readOnly value={task.text} />
           </div>
         ))}
+        <AddTaskForm list={list} onAddTask={onAddTask}/>
       </div>
     </div>
   );
